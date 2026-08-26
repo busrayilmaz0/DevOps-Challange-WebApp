450aed2e493fc63e2d8ecab033265b563b4f5e8d
Büşra <busra04.yilmaz@gmail.com>
2026-08-26 17:50:52 +0300

    Statik dosyalar ve arayuz aktif edildi

--- ZiraatWebApp/Program.cs
+++ ZiraatWebApp/Program.cs
@@ -1,9 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.WebHost.UseUrls("http://localhost:52369");
-
 var app = builder.Build();
 
-app.MapGet("/", () => "Hoş Geldiniz");
+// Statik dosyaları (wwwroot içindeki HTML, CSS, resimler) aktif eder
+app.UseDefaultFiles(); // index.html'i otomatik olarak ana sayfa yapar
+app.UseStaticFiles();
 
 app.Run();
